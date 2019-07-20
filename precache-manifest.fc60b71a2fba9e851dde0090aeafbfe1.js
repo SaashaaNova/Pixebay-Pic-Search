c3c444a7257e4ca152295a21b48cf926adff8a25
SasshaaNova <aa.niewinska@gmail.com>
2019-07-20 18:27:16 +0200

    Updates

---
+++ precache-manifest.fc60b71a2fba9e851dde0090aeafbfe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501538112c0a669bee30bbf28bc536a3",
+    "url": "/Pixebay-Pic-Search/index.html"
+  },
+  {
+    "revision": "461081acfb02b45d974d",
+    "url": "/Pixebay-Pic-Search/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Pixebay-Pic-Search/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "461081acfb02b45d974d",
+    "url": "/Pixebay-Pic-Search/static/js/main.2e7294e1.chunk.js"
+  },
+  {
+    "revision": "c63b598e77175a81c3e0",
+    "url": "/Pixebay-Pic-Search/static/js/runtime~main.3ed603f9.js"
+  }
+]);
